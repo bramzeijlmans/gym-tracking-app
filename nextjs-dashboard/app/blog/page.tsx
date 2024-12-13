08ac63adbaa74edd153fccd75163c8607d876334
Bram Zeijlmans <bramzeijlmans@gmail.com>
2024-12-13 13:34:33 +0100

    Add files via upload

---
+++ nextjs-dashboard/app/blog/page.tsx
@@ -0,0 +1,29 @@
+
+export default async function Page() {
+    const posts = [
+        {
+            "createdAt": "2024-12-13T04:04:14.947Z",
+            "name": "Levi Kassulke",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/986.jpg",
+            "id": "1"
+        },
+        {
+            "createdAt": "2024-12-12T08:23:03.196Z",
+            "name": "Miss Ed Collier",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1070.jpg",
+            "id": "2"
+        },
+        {
+            "createdAt": "2024-12-12T08:25:10.791Z",
+            "name": "Carmen Mosciski",
+            "avatar": "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/927.jpg",
+            "id": "3"
+        }
+    ]
+
+    return (
+        <ul>
+            <li>Hello world</li>
+        </ul>
+    )
+}
---
+++ nextjs-dashboard/app/settings/page.tsx
@@ -0,0 +1,8 @@
+import styles from '@/app/ui/home.module.css';
+import {inter, lusitana} from "@/app/ui/fonts";
+
+export default function Page() {
+    return <div>
+        <p>Settings Page</p>
+    </div>;
+}
